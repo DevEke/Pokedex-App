5cf00f5fca8e97fc3d73bc6bca134f58d8c2c1c8
Chris Eke <chriseke13@gmail.com>
2021-01-15 11:29:15 -0600

    Cleared script to begin pokedex app

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,1 @@
-alert('Hello World');
 
-let favoriteFoods = [
-  ' chicken',
-  ' pasta',
-  ' salads',
-  ' broccoli',
-  ' apples',
-  ' and bananas.'
-];
-
-document.write('My favorite foods are' + favoriteFoods);
