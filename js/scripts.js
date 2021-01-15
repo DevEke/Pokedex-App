44535f2f89cd0ca82d2e3890e564f7cd1791df8d
Chris Eke <chriseke13@gmail.com>
2021-01-15 12:45:09 -0600

    Added height property to all objects in pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -4,46 +4,55 @@ let pokemonList = [
   {
     id: 001,
     name: 'Bulbasaur',
-    type: ['grass', 'poison']
+    type: ['grass', 'poison'],
+    height: 2.33
   },
   {
     id: 002,
     name: 'Ivysaur',
-    type: ['grass', 'poison']
+    type: ['grass', 'poison'],
+    height: 3.25
   },
   {
     id: 003,
     name: 'Venusaur',
-    type: ['grass', 'poison']
+    type: ['grass', 'poison'],
+    height: 6.58
   },
   {
     id: 004,
     name: 'Charmander',
-    type: ['fire']
+    type: ['fire'],
+    height: 2
   },
   {
     id: 005,
     name: 'Charmeleon',
-    type: ['fire']
+    type: ['fire'],
+    height: 3.58
   },
   {
     id: 006,
     name: 'Charizard',
-    type: ['fire', 'flying']
+    type: ['fire', 'flying'],
+    height: 5.58
   },
   {
     id: 007,
     name: 'Squirtle',
-    type: ['water']
+    type: ['water'],
+    height: 1.67
   },
   {
     id: 008,
     name: 'Wartortle',
-    type: ['water']
+    type: ['water'],
+    height: 3.25
   },
   {
     id: 009,
     name: 'Blastoise',
-    type: ['water']
+    type: ['water'],
+    height: 5.25
   }
 ];
